04aef09287fcba8160e437ce98e3e789675c1ad8
MattMatt2000 <mconstanshu@gmail.com>
2017-10-05 00:44:17 +0200

    Fixed core layout

---
+++ Source/KWSub.Core/Compressors/LZ4Compressor.cs
@@ -0,0 +1,25 @@
+﻿using LZ4;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWSub.Core.Compressors
+{
+    public class LZ4Compressor : ICompressor
+    {
+        public void WriteData(Stream outputStream, byte[] data)
+        {
+            using (var lz4Stream = new LZ4Stream(outputStream, LZ4StreamMode.Compress))
+                lz4Stream.Write(data, 0, data.Length);
+        }
+
+        public byte[] ReadData(Stream inputStream)
+        {
+            // MattMatt2000: Ha, I had to go to sleep... sry ;-;;
+            return new byte[] { };
+        }
+    }
+}
---
+++ Source/KWSub.Core/ICompressor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KWSub.Core
+{
+    public interface ICompressor
+    {
+        void    WriteData(Stream outputStream, byte[] data);
+        byte[]  ReadData(Stream inputStream);
+    }
+
+}
